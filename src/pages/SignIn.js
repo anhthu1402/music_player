faff57e9c9451158b0cf408722d1f2d6464ac782
AnTTViiii <20520856@gm.uit.edu.vn>
2023-05-06 20:34:05 +0700

    modify page SignIn.js, add page SignIn.css

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/SignIn.css";
 
 function SignIn() {
-  return <div>jhgfds</div>;
+  return (
+    <div className="signinContainer">
+      <h1>Đăng nhập</h1>
+      <div className="signinGoogle">
+        <Link to={"#"}>Tiếp tục với Google</Link>
+      </div>
+      <p className="dividingLine"><span>HOẶC</span></p>
+      <form className="signinForm">
+        <div className="div1">
+          <p>Email</p>
+          <input name="userEmail" type="email" placeholder="example@gmail.com"/>
+        </div>
+        <div className="div1">
+          <p>Mật khẩu</p>
+          <input name="userPassword" type="password" placeholder="example@gmail.com"/>
+        </div>
+        <div className="div3">
+          <div>
+            <input alignItems='left' type="checkbox" name="rememberAccount"/>
+            <p>Nhớ tên tài khoản</p>
+          </div>
+          <p><Link to={"#"}>Quên mật khẩu?</Link></p>
+        </div>
+        <input type="submit" name="submitSignin" className="submitSignin" value={"Đăng nhập"}/>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
