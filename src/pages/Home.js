f5d0688edb3114ccb8a8b765d6398d65f59a7c2d
AnTTViiii <20520856@gm.uit.edu.vn>
2023-05-10 07:53:43 +0700

    add item component for playlists at Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import RecentlyPlaylist from "../components/RecentlyPlaylist";
 import TopTrendingArtist from "../components/TopTrendingArtist";
 import "../styles/Home.css";
 import MusicPlayerContext from "../MusicPlayerContext";
+import {PlaylistData} from '../components/Data/PlaylistData';
+import HomePlaylistItem from "../components/Item/HomePlaylistItem";
 
 function Home() {
   const song = useContext(MusicPlayerContext);
@@ -21,11 +23,34 @@ function Home() {
       <RecentlyPlaylist />
 
       <div className="categoryHeader">
+        <p className="categoryTitle">Đề xuất cho bạn</p>
+      </div>
+      <div className='recommendationPlaylist'>
+        {PlaylistData.map((item, key) => key < 4 && (
+          <div className="listPlaylists">
+            <HomePlaylistItem key={key} item={item}/>
+            <div className="artists">
+              {item.playlistSongs.map((item2, key2) => (
+                <span>
+                  {item2.artist.map((child, index) => {
+                    return (
+                      <span key={index} item={child} className="artist">
+                        <Link to={`/artist/${child.name}`} state={child}>{child.name}</Link>
+                      </span>
+                    );
+                  })}
+                </span>
+              ))}
+             </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="categoryHeader">
         <p className="categoryTitle">Nghệ sỹ thịnh hành</p>
       </div>
       <TopTrendingArtist />
-      {/* RecentlyListen là trang ta dùng để nháp trc khi tạo page ms thoi á. mốt xóa. */}
-      {/* <RecentlyListen /> */}
+
     </div>
   );
 }
