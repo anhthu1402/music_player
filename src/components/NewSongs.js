63938054df93e9e838fdd26ea3907305809e66c9
AnTTViiii <20520856@gm.uit.edu.vn>
2023-05-17 10:45:10 +0700

    modify new song header toggle tab.

--- src/components/NewSongs.js
+++ src/components/NewSongs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useContext } from "react";
 import MusicPlayerContext from "../MusicPlayerContext";
 import { FavoriteBorderOutlined, MoreHoriz, PlayCircleFilled } from "@mui/icons-material";
@@ -8,22 +8,47 @@ import '../styles/NewSongs.css';
 function NewSongs() {
   const tracks = SongData;
   const song = useContext(MusicPlayerContext);
-  const [params, setParams] = useState(false);
 
-  const handleChangeTab = () => {
-    setParams(!params);
+  const [toggleState, setToggleState] = useState(1);
+  const datas = [];
+  const toggleTab = (index) => {
+    setToggleState(index);
+    if (index === 1) {
+      datas = SongData.map();
+    }
+    if (index === 2) {
+
+    }
   };
+
+  // const [loading, setLoading] = useState(true);
+  // const [records, setRecords] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos/")
+  //   .then(response => response.json())
+  //   // .then(json => console.log(json))
+  //   .then(data => setRecords(data))
+  //   .catch(error => console.error(error));
+  //   setLoading(false);
+  // }, []);
+
   return (
     <div className='listSongContainer'>
       <div className='listSongHeader'>
-        <div className='nation active'>Tất cả</div>
-        <div className='nation'>Việt Nam</div>
-        <div className='nation'>US-UK</div>
-        <div className='nation'>Châu Á</div>
-        <div className='nation'>Khác</div>
+        <div className={toggleState === 1 ? 'nation active' : "nation"}
+          onClick={() => toggleTab(1)}>Tất cả</div>
+        <div className={toggleState === 2 ? 'nation active' : "nation"}
+          onClick={() => toggleTab(2)}>Việt Nam</div>
+        <div className={toggleState === 3 ? 'nation active' : "nation"}
+          onClick={() => toggleTab(3)}>US-UK</div>
+        <div className={toggleState === 4 ? 'nation active' : "nation"}
+          onClick={() => toggleTab(4)}>Châu Á</div>
+        <div className={toggleState === 5 ? 'nation active' : "nation"}
+          onClick={() => toggleTab(5)}>Khác</div>
       </div>
       <div className="listSongBody">
-        {SongData.map((item, key) => key < 9 && 
+        {SongData.map((item, key) => item.countryId === 1 && key < 9 && 
           <div class='track'>
             <TrackItem item={item} key={key} className='trackItem' />
             <div className='fauxImg'>
@@ -41,6 +66,15 @@ function NewSongs() {
       </div>
       
     </div>
+    // <div>
+    //   {loading ? "Loading..." : (
+    //     <div>
+    //       {records && records.map((item, key) => (
+    //         <p>{item.title}</p>
+    //       ))}
+    //     </div>
+    //   )}
+    // </div>
   )
 }
 
