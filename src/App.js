a691ad56d5d9a6771b16a87d249ec76181289169
anhthu1402 <20520792@gm.uit.edu,vn>
2023-06-15 13:43:14 +0700

    test api

--- src/App.js
+++ src/App.js
@@ -79,6 +79,7 @@ function App() {
       ],
     },
   ]);
+
   return (
     <SidebarProvider>
       <MusicPlayerProvider>
--- src/components/AppRoot.jsx
+++ src/components/AppRoot.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import MusicPlayer from "./MusicPlayer";
 import AppAppBar from "./AppAppBar";
+import axios from "axios";
 
 function AppRoot() {
+  const [message, setMessage] = useState([]);
+  const id = 1;
+  useEffect(() => {
+    axios.get("/api/getPlaylistById/" + id).then((result) => {
+      const playlists = result.data.playlist;
+      setMessage(playlists);
+    });
+  });
+  // useEffect(() => {
+  //   axios.get("/api/getAllSongs").then((res) => {
+  //     const playlists = res.data.songs;
+  //     setMessage(playlists);
+  //   });
+  // });
   return (
     <>
       <div style={{ position: `fixed`, top: `0`, left: 0 }}>
@@ -21,6 +36,10 @@ function AppRoot() {
       >
         <AppAppBar />
         <div>
+          {message.map((item) => (
+            <div key={item.id}>{item.playlistName}</div>
+          ))}
+
           <Outlet />
         </div>
         <div
