ec2f9370cd92cf0257e38511a8ecdff4aeeb6cbb
AnTTViiii <20520856@gm.uit.edu.vn>
2023-06-18 14:32:53 +0700

    search

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,50 +1,122 @@
-import React, {useState}from 'react'
-import '../styles/InputSearch.css'
-import { SearchBarData } from './Data/SearchBarData';
-
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/InputSearch.css';
+import { AlbumData } from './Data/AlbumData';
+import { ArtistsData } from './Data/ArtistsData';
+import axios from "axios";
+import TrackItem from './Item/TrackItem';
+import NotificationContext from "../NotificationContext";
+import MusicPlayerContext from '../MusicPlayerContext';
 function InputSearch() {
-    const [searchInput, setSearchInput] = useState("");
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-      };
-      
-      if (searchInput.length > 0) {
-          SearchBarData.filter((i) => {
-          return i.value.match(searchInput.toLocaleLowerCase);
-      });
+    const song = useContext(MusicPlayerContext);
+    const notification=useContext(NotificationContext);
+    const [searchTerm, setSearchTerm] = useState("");
+    const rsSong = [], playlist = [], rsArtist = [], rsAlbum = [];
+    if (searchTerm !== '') {
+        AlbumData.map((value) => {
+            value.songs.map((val, key) => {
+                if (val.songName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                    rsSong.push(val);
+                    playlist.push(value);
+                }
+            })
+        });
+        ArtistsData.map((val) => {
+            if (val.artistName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                rsArtist.push(val);
+            }
+        });
+        AlbumData.map((val) => {
+            if (val.albumName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                rsAlbum.push(val)
+            }
+        });
     }
-
-    const search = document.getElementById('search');
-    const results = document.getElementById('results');
-    let search_term = '';
-
+    
     return (
         <div className='inputSearchContainer'>
-            <form class="nosubmit">
+            <div class="nosubmit">
                 <input class="nosubmit" type="search" 
                         placeholder="Tìm kiếm tên bài hát, nghệ sĩ, ..."
-                        onChange={handleChange} value={searchInput} />
-            </form>
-            <ul className='searchResults'>
-                {SearchBarData.map((results, index) => {
-                    <li>{results.name}</li>
-                })}
-            </ul>
-            {/* <table>
-                <tr>
-                    <th>Key</th>
-                    <th>Value</th>
-                </tr>
-                {SearchBarData.map((data, index) => {
-                    <div>
-                        <tr>
-                            <td>{data.key}</td>
-                            <td>{data.value}</td>
-                        </tr>
-                    </div>
-                })}
-            </table> */}
+                        onChange={(event) => {
+                            setSearchTerm(event.target.value);
+                        }}
+                        onkeydown="return event.key != 'Enter';" />
+            </div>
+            <div className={searchTerm !== "" ? 'resultSearch' : 'hideRS'}>
+                {/* <div id='headingSearchSong'>Bài hát</div>
+                <div className='rs_songs'>
+                    {AlbumData.map((value) => {
+                        value.songs.map((val, key) => {
+                            if (val.songName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return (
+                                    <div className="song">
+                                        <TrackItem
+                                            key={key}
+                                            item={val}
+                                            tracks={value.songs}
+                                            index={key}
+                                            song={song} 
+                                            notification={notification}
+                                        />
+                                    </div>
+                                
+                                );
+                            }
+                        })}
+                    )}
+                </div> */}
+                {rsSong === null ? '' : 
+                `<><div id='headingSearchSong'>Bài hát</div>
+                <div className='rs_songs'>
+                    rsSong.map((val, key) => {
+                        return (
+                            <div className="song">
+                                <TrackItem
+                                    key={key}
+                                    item={val}
+                                    tracks={playlist.songs}
+                                    index={key}
+                                    song={song} 
+                                    notification={notification}
+                                />
+                            </div>
+                        )
+                    })
+                    </div></>`
+                }
+                <div id='headingSearchArtist'>Nghệ sỹ</div>
+                <div className='artistsResult'>
+                    {ArtistsData.map((val) => {
+                        if (val.artistName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return (
+                                <div className="artistResult">
+                                    <img src={val.artistImage} alt={val.artistName} />
+                                    <div className='result-details'>
+                                        <p><Link to={`/artist/${val.artistName}`} state={val}>{val.artistName}</Link></p>
+                                        <p>{val.numberOfFollower}</p>
+                                    </div>
+                                </div>
+                            );
+                        }
+                        })
+                    }
+                </div>
+                <div id='headingSearchAlbum'>Album</div>
+                <div className='albumsResult'>
+                    {AlbumData.map((val) => {
+                        if (val.albumName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return (
+                                <div className="albumResult">
+                                    <img src={val.albumImage} alt={val.albumName} />
+                                    <p className='rs_albumName'><Link to={`/album/${val.albumName}`} state={val.id}>{val.albumName}</Link></p>
+                                </div>
+                            );
+                        }
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
