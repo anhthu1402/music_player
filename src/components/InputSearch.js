4875823c6eed940210099c3b500e2bd97099019d
AnTTViiii <20520856@gm.uit.edu.vn>
2023-06-18 14:54:35 +0700

    modify search

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -66,56 +66,62 @@ function InputSearch() {
                         })}
                     )}
                 </div> */}
-                {rsSong === null ? '' : 
-                `<><div id='headingSearchSong'>Bài hát</div>
-                <div className='rs_songs'>
-                    rsSong.map((val, key) => {
-                        return (
-                            <div className="song">
-                                <TrackItem
-                                    key={key}
-                                    item={val}
-                                    tracks={playlist.songs}
-                                    index={key}
-                                    song={song} 
-                                    notification={notification}
-                                />
-                            </div>
-                        )
-                    })
-                    </div></>`
-                }
-                <div id='headingSearchArtist'>Nghệ sỹ</div>
-                <div className='artistsResult'>
-                    {ArtistsData.map((val) => {
-                        if (val.artistName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                {(rsSong.length <= 0 && rsArtist.length <= 0 && rsAlbum.length <= 0) ? 
+                    <p align="center"><i>Không tìm thấy!</i></p> : '' }
+                {rsSong.length <= 0 ? '' : <>
+                    <div id='headingSearchSong'>Bài hát</div>
+                    <div className='rs_songs'>
+                        {rsSong.map((val, key) => {
                             return (
-                                <div className="artistResult">
-                                    <img src={val.artistImage} alt={val.artistName} />
-                                    <div className='result-details'>
-                                        <p><Link to={`/artist/${val.artistName}`} state={val}>{val.artistName}</Link></p>
-                                        <p>{val.numberOfFollower}</p>
+                                <div className="song">
+                                    <TrackItem
+                                        key={key}
+                                        item={val}
+                                        tracks={playlist.songs}
+                                        index={key}
+                                        song={song} 
+                                        notification={notification}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>}
+                {rsArtist.length <= 0 ? '' : <>
+                    <div id='headingSearchArtist'>Nghệ sỹ</div>
+                    <div className='artistsResult'>
+                        {rsArtist.map((val) => {
+                            if (val.artistName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return (
+                                    <div className="artistResult">
+                                        <img src={val.artistImage} alt={val.artistName} />
+                                        <div className='result-details'>
+                                            <p><Link to={`/artist/${val.artistName}`} state={val}>{val.artistName}</Link></p>
+                                            <p>{val.numberOfFollower}</p>
+                                        </div>
                                     </div>
-                                </div>
-                            );
+                                );
+                            }
+                            })
                         }
-                        })
-                    }
-                </div>
-                <div id='headingSearchAlbum'>Album</div>
-                <div className='albumsResult'>
-                    {AlbumData.map((val) => {
-                        if (val.albumName.toLowerCase().includes(searchTerm.toLowerCase())) {
-                            return (
-                                <div className="albumResult">
-                                    <img src={val.albumImage} alt={val.albumName} />
-                                    <p className='rs_albumName'><Link to={`/album/${val.albumName}`} state={val.id}>{val.albumName}</Link></p>
-                                </div>
-                            );
+                    </div>
+                </>}
+                {rsAlbum.length <= 0 ? '' : <>
+                    <div id='headingSearchAlbum'>Album</div>
+                    <div className='albumsResult'>
+                        {rsAlbum.map((val) => {
+                            if (val.albumName.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                return (
+                                    <div className="albumResult">
+                                        <img src={val.albumImage} alt={val.albumName} />
+                                        <p className='rs_albumName'><Link to={`/album/${val.albumName}`} state={val.id}>{val.albumName}</Link></p>
+                                    </div>
+                                );
+                            }
+                            })
                         }
-                        })
-                    }
-                </div>
+                    </div>
+                </>}
             </div>
         </div>
     )
